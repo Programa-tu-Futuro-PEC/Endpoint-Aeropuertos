11250f6f921ffe56dcfc074741257bcc740f80c7
Carlos Marin <carlosmarind@gmail.com>
2020-05-06 16:02:09 -0400

    mock de lista de aeropuertos

--- src/main/java/cl/uchile/dcc/vuelos/dao/AeropuertoDAO.java
+++ src/main/java/cl/uchile/dcc/vuelos/dao/AeropuertoDAO.java
@@ -9,8 +9,10 @@ import java.util.List;
 public class AeropuertoDAO {
 
     static public List<Aeropuerto> obtenerAeropuertos(){
-        try {
-            Connection conn = ConnectionManager.obtenerConexion();
+        List<Aeropuerto> resultado = MockAeropuerto.listaAeropuertos();
+        return resultado;
+       /*  try {
+           Connection conn = ConnectionManager.obtenerConexion();
             PreparedStatement ps = conn.prepareStatement("select top 10 id, nombre, pais, ciudad, iata, icao from aeropuerto");
             List<Aeropuerto> resultado = new ArrayList<>();
             ResultSet rs = ps.executeQuery();
@@ -26,12 +28,23 @@ public class AeropuertoDAO {
             }
             return resultado;
         } catch (SQLException e) {
+            System.out.println("Error al consultar los aeropuertos");
+            e.printStackTrace();
             return null;
-        }
+        }*/
     }
 
     static public Aeropuerto obtenerAeropuertoPorID(long id){
-        try {
+
+        List<Aeropuerto> resultado = MockAeropuerto.listaAeropuertos();
+        for(Aeropuerto aeropuerto : resultado){
+            if (aeropuerto.getId() == id){
+                return aeropuerto;
+            }
+        }
+        return null;
+
+      /*  try {
             Connection conn = ConnectionManager.obtenerConexion();
             PreparedStatement ps = conn.prepareStatement("select nombre, pais, ciudad, iata, icao from aeropuerto where id = ?");
             ps.setLong(1, id);
@@ -44,9 +57,9 @@ public class AeropuertoDAO {
             String icao = rs.getString("icao");
             return new Aeropuerto(id, nombre, ciudad, pais, iata, icao);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Error al consultar el aeropuerto");
             return null;
-        }
+        }*/
     }
 
 }
---
+++ src/main/java/cl/uchile/dcc/vuelos/dao/MockAeropuerto.java
@@ -0,0 +1,24 @@
+package cl.uchile.dcc.vuelos.dao;
+
+import cl.uchile.dcc.vuelos.model.Aeropuerto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MockAeropuerto {
+
+    public static List<Aeropuerto> listaAeropuertos = new ArrayList<Aeropuerto>(
+            Arrays.asList(
+                    new Aeropuerto(0,"scl","santiago","chile","scli","sclo"),
+                    new Aeropuerto(1,"axm","Barranquilla","colombia","axmi","axmo"),
+                    new Aeropuerto(2,"bog","bogota","colombia","bogi","bogo")
+            )
+    );
+
+    public static List<Aeropuerto> listaAeropuertos(){
+        return listaAeropuertos;
+    }
+
+
+}
